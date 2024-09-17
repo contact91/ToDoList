d438b3bfd776af0092f467042f585d1b73e6575c
contact91 <chouvelquentin91@gmail.com>
2024-09-17 22:45:26 +0200

    check button added

--- script.js
+++ script.js
@@ -4,16 +4,30 @@ const button = document.getElementById("button");
 
 function addOnList(event) {
   event.preventDefault();
+
   const newTask = document.createElement("div");
+  newTask.innerText = `${input.value}`;
+
   const trash = document.createElement("i");
   trash.classList.add("fa-solid", "fa-trash");
-  newTask.innerText = `${input.value}`;
   newTask.append(trash);
+
+  const checked = document.createElement("i");
+  checked.classList.add("fa-solid", "fa-check");
+  newTask.append(checked);
+
   form.append(newTask);
+
   input.value = "";
   trash.addEventListener("click", function () {
     newTask.remove();
   });
+  checked.addEventListener(`click`, () => {
+    if (newTask.style.textDecoration === "line-through") {
+      newTask.style.textDecoration = "none";
+    } else {
+      newTask.style.textDecoration = "line-through";
+    }
+  });
 }
-
 button.addEventListener(`click`, addOnList);
